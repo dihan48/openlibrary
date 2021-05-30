7df4d74f03c984920db99579795b4e8d6dd6a099
dihan48 <dihan4894@mail.ru>
2021-05-30 21:59:16 +0300

    удаление не нужных файлов шаблона create react app

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import headerReducer from '../slices/searchSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     search: headerReducer,
   },
 });
