82f2b48717cb6bc751c2158c4b4dd774e297b4c7
dihan48 <dihan4894@mail.ru>
2021-10-11 01:10:21 +0300

    Update openlibraryAPI.js

--- src/openlibraryAPI/openlibraryAPI.js
+++ src/openlibraryAPI/openlibraryAPI.js
@@ -1,7 +1,7 @@
 
 export function fetchSearch(value = '', page = 1) {
   return new Promise((resolve) => {
-    fetch('http://openlibrary.org/search.json?q=' + encodeURI(value) + '&page=' + page)
+    fetch('https://openlibrary.org/search.json?q=' + encodeURI(value) + '&page=' + page)
       .then(response => response.json())
       .then(data => {
         resolve({ data: data?.docs, value, page, start: data.start })
